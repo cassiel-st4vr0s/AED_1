a7a240636a398f4f2fd277ff20e6fa94c0133f59
cassiel-st4vr0s <111382100+cassiel-st4vr0s@users.noreply.github.com>
2023-06-12 20:46:32 -0400

    main_insert.c

--- main_insert.c
+++ main_insert.c
@@ -23,7 +23,7 @@ int main() {
 
         printf("Vetor ordenado em ordem crescente:\n");
         for (i = 0; i < n; i++) {
-                printf("%d ", vetor[i]);
+                printf("%d \n", vetor[i]);
 
         }
 
